70e290c494f0dcc74f4c9bc2746589153cfa1c0a
Medformatik <dev@fwcr.de>
2025-04-03 00:25:13 +0200

    Remove old implementation

--- lib/list_equality.dart
+++
@@ -1,9 +0,0 @@
-class ListEquality<T> {
-  bool equals(List<T> a, List<T> b) {
-    if (a.length != b.length) return false;
-    for (int i = 0; i < a.length; i++) {
-      if (a[i] != b[i]) return false;
-    }
-    return true;
-  }
-}
